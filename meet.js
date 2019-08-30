91e9fde53e9aeb91cc5092e43da3b7b96d731c9d
Glitch (hello-express) <none>
2019-08-30 10:33:06 +0000

    🎫🍯 Checkpoint ./server.js:2253437/1046 ./package.json:2253437/1334 ./google-api.js:2253437/3021 ./meet.js:2253437/1383

---
+++ meet.js
@@ -0,0 +1,52 @@
+const { google } = require("googleapis");
+const { login } = require("./google-api");
+
+function addMinutes(date, minutes) {
+  return new Date(date.getTime() + minutes * 60000);
+}
+
+const createEvent = async auth => {
+  const calendar = google.calendar({ version: "v3", auth });
+
+  var event = {
+    summary: "Call",
+    start: {
+      dateTime: new Date().toISOString(),
+      timeZone: "Europe/Paris"
+    },
+    end: {
+      dateTime: addMinutes(new Date(), "15").toISOString(),
+      timeZone: "Europe/Paris"
+    }
+
+    // attendees: [{ email: "benjamin.levesque@request.network" }, { email: "sbrin@example.com" }],
+    // reminders: {
+    //   useDefault: false,
+    //   overrides: [
+    //     { method: "email", minutes: 24 * 60 },
+    //     { method: "popup", minutes: 10 }
+    //   ]
+    // }
+  };
+
+  const eventCreateResponse = await calendar.events.insert({
+    auth: auth,
+    calendarId: "primary",
+    resource: event
+  });
+
+  calendar.events.patch({
+    calendarId: "primary",
+    eventId: eventCreateResponse.data.id,
+    resource: {
+      conferenceData: {
+        createRequest: { requestId: "7qxalsvy0e" }
+      }
+    },
+    conferenceDataVersion: 1
+  });
+};
+
+module.exports ={
+  createEvent
+};
--- server.js
+++ server.js
@@ -1,22 +1,23 @@
-// server.js
-// where your node app starts
-
-// init project
 const express = require('express');
 const app = express();
+const {createEvent} = require("./meet");
 
-// we've started you off with Express, 
-// but feel free to use whatever libs or frameworks you'd like through `package.json`.
-
-// http://expressjs.com/en/starter/static-files.html
-app.use(express.static('public'));
+app.use(express.json());
 
 // http://expressjs.com/en/starter/basic-routing.html
-app.get('/', function(request, response) {
-  response.sendFile(__dirname + '/views/index.html');
+app.get('/', (request, response) =>{
+  response.send('it is working');
 });
 
+app.post('/', async (req, res)=>{
+  await createEvent();
+  res.send({
+    response_type: 'in_channel', // public to the channel
+    text: 'URL'
+  });
+})
+
 // listen for requests :)
-const listener = app.listen(process.env.PORT, function() {
+const listener = app.listen(process.env.PORT, ()=> {
   console.log('Your app is listening on port ' + listener.address().port);
 });
